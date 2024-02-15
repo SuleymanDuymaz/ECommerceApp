702f4973e605dbb04aba6cdba39835a295443169
Süleyman <sduymaz8@gmail.com>
2024-02-15 12:48:29 +0300

    Alertify service

--- ECommerceAppClient/ECommerceAppClient/src/app/app.component.ts
+++ ECommerceAppClient/ECommerceAppClient/src/app/app.component.ts
@@ -1,14 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertifyService, MessageType, Position } from './services/admin/alertify.service';
 declare var $:any
+//declare var alertify:any
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ECommerceAppClient';
+  /**
+   *
+   */
+  constructor(private alertify:AlertifyService) {
+  
+    
+  }
+  ngOnInit(): void {
+    this.alertify.message("Merhaba",{
+messageType:MessageType.Success,
+delay:3,
+position:Position.TopRight
+
+    });
+  }
+  createMessage(){
+    this.alertify.message("Merhaba",{
+      messageType:MessageType.Success,
+      delay:5,
+
+    });
+  }
+  dissMissMessage(){
+    this.alertify.dismiss()
+
+  }
+
+
+
 }
 $(document).ready(()=>{
-  alert("Hoşgeldin")
+  alert("Angular JS ile yazılan projeye HOŞGELDİN")
+  //alertify.alert('Ready!')
+
 })
